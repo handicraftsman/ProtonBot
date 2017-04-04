d7b493a554f0fa956adbbf9ad3cad3a12bce5336
Nickolay <nickolay02@inbox.ru>
2017-04-04 23:17:12 +0300

    Added colors for CTCP VERSION

--- lib/protonbot/core_plugin/hooks/ctcp.rb
+++ lib/protonbot/core_plugin/hooks/ctcp.rb
@@ -1,7 +1,8 @@
 hook(type: :ctcp, cmd: 'VERSION') do |dat|
-  dat[:plug].nctcp(dat[:nick], %(VERSION ProtonBot v#{ProtonBot::VERSION} on Ruby ) +
-    %(v#{RUBY_VERSION}.#{RUBY_PATCHLEVEL}/#{RUBY_PLATFORM} and Heliodor ) +
-    %(v#{Gem.loaded_specs['heliodor'].version.version}))
+  dat[:plug].nctcp(dat[:nick], %(VERSION ) + 
+    %(%B%C%BLUEProtonBot v#{ProtonBot::VERSION}%N ) +
+    %(on %B%C%REDRuby v#{RUBY_VERSION}.#{RUBY_PATCHLEVEL}/#{RUBY_PLATFORM}@#{RUBY_ENGINE}#{RUBY_ENGINE_VERSION}%N ) +
+    %(and %B%C%YELLOWHeliodor v#{Gem.loaded_specs['heliodor'].version.version}%N))
 end
 
 hook(type: :ctcp, cmd: 'PING') do |dat|
